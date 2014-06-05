4148510a1878c05ea6aae1fa9412d195d150ac06
valyouw <valyouw@gmail.com>
2014-06-05 12:53:00 +0300

    Add better stack tracing - not done yet, just backup...

--- test/general.spec.js
+++ test/general.spec.js
@@ -1,5 +1,7 @@
 var njsTrace = require('../njsTrace.js');
-njsTrace.inject({logger: false, enabled: true, wrapFunctions:true, prof:false, trace:true});
+njsTrace.inject({logger: false, enabled: true, wrapFunctions:false, prof:false, trace:true});
 
-var file1 = require('./mocks/file1.js');
-file1.foo();
+//var file1 = require('./mocks/file1.js');
+var file2 = require('./mocks/file2.js');
+//file1.foo();
+file2.foo();
---
+++ test/mocks/file2.js
@@ -0,0 +1,30 @@
+function a() {
+	b();
+}
+
+function b() {
+	try {
+		c();
+	} catch (e) {
+		console.log('ERR:', e);
+	}
+	ba();
+}
+
+function c() {
+	d();
+}
+
+function d() {
+	throw new Error('Error from d');
+}
+
+function ba() {}
+
+exports.foo = function() {
+	try {
+		a();
+	} catch (e) {
+		console.log('ERROR:', e);
+	}
+}
